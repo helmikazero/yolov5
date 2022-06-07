bfac7c996a70e8dad9ceba1476c29fa2289ab2c6
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2022-06-07 22:04:18 +0700

    newgen

--- muantap.py
+++ muantap.py
@@ -3,112 +3,48 @@ import torch
 import numpy as np
 
 
-
-
 imgsz = (240,240)
 
 stream = cv2.VideoCapture(0)
-customyolov5s = torch.hub.load('','custom', path='weightHedect/hedec_yolov5s.pt', source='local')
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-themodel = customyolov5s.to(device)
+customyolov5s = torch.hub.load('','custom', path='weightHedect/nexgen_hedec_s1.pt', source='local')
 
 def score_frame(frame,model):
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    # model.to(device)
-    # frame = [torch.tensor(frame)]
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
     results = model(frame)
-    labels = results.xyxyn[0][:, -1].numpy()
-    cord = results.xyxyn[0][:, :-1].numpy()
+    mantap = results.pandas().xyxy[0]
+    
+    return mantap
 
-    return labels,cord
-
-
-def plot_boxes(results,frame,model):
-    labels,cord = results
-    n = len(labels)
-    x_shape, y_shape = frame.shape[1], frame.shape[0]
-
-    for i in range(n):
-        row = cord[i]
-
-        if row[4] < 0.2: 
-            continue
-        x1 = int(row[0]*x_shape)
-        y1 = int(row[1]*y_shape)
-        x2 = int(row[2]*x_shape)
-        y2 = int(row[3]*y_shape)
-        bgr = (0, 255, 0) # color of the box
-        classes = model.names # Get the name of label index
-        label_font = cv2.FONT_HERSHEY_SIMPLEX #Font for the label.
-        cv2.rectangle(frame, \
-                      (x1, y1), (x2, y2), \
-                       bgr, 2) #Plot the boxes
-        cv2.putText(frame,\
-                    classes[labels[i]], \
-                    (x1, y1), \
-                    label_font, 0.9, bgr, 2) #Put a label over box.
-        return frame
-
-# def __call__(self):
-#     player = self.get_video_stream() #Get your video stream.
-#     assert player.isOpened() # Make sure that their is a stream. 
-#     #Below code creates a new video writer object to write our
-#     #output stream.
-#     x_shape = int(player.get(cv2.CAP_PROP_FRAME_WIDTH))
-#     y_shape = int(player.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#     four_cc = cv2.VideoWriter_fourcc(*"MJPG") #Using MJPEG codex
-#     out = cv2.VideoWriter(out_file, four_cc, 20, \
-#                           (x_shape, y_shape)) 
-#     ret, frame = player.read() # Read the first frame.
-#     while rect: # Run until stream is out of frames
-#         start_time = time() # We would like to measure the FPS.
-#         results = self.score_frame(frame) # Score the Frame
-#         frame = self.plot_boxes(results, frame) # Plot the boxes.
-#         end_time = time()
-#         fps = 1/np.round(end_time - start_time, 3) #Measure the FPS.
-#         print(f"Frames Per Second : {fps}")
-#         out.write(frame) # Write the frame onto the output.
-#         ret, frame = player.read() # Read next frame.
-
+def printTheBoxYEAAAH(img,predresultinpandas):
+    printed_img = img
+    for i in range(len(predresultinpandas)):
+        printed_img = cv2.rectangle(printed_img, (int(predresultinpandas['xmin'][i]),int(predresultinpandas['ymin'][i])),(int(predresultinpandas['xmax'][i]),int(predresultinpandas['ymax'][i])),(0, 255, 0),2)
+        printed_img = cv2.putText(printed_img,predresultinpandas['name'][i],(int(predresultinpandas['xmin'][i]),int(predresultinpandas['ymin'][i])),cv2.FONT_HERSHEY_SIMPLEX,0.9,(0, 255, 0),2)
+        printed_img = cv2.putText(printed_img,str(predresultinpandas['confidence'][i]),(int(predresultinpandas['xmax'][i]),int(predresultinpandas['ymax'][i])),cv2.FONT_HERSHEY_SIMPLEX,0.9,(0, 255, 0),2)
+    return printed_img
 
 def main():
     while True:
         ret_val, img = stream.read()
 
-        # img = cv2.resize(img,imgsz, interpolation=cv2.INTER_AREA) 
-
         img = crop_image_square(img)
 
-        img = cv2.resize(img, imgsz)
-
         results = score_frame(img,customyolov5s)
-
-        # img = plot_boxes(results,img,customyolov5s)
+        img = printTheBoxYEAAAH(img,results)
 
         cv2.imshow('Frame',img)
 
         if cv2.waitKey(1) == 27:
             break
 
-# def main():
-#     ret_val, img = stream.read()
-
-#     crop_image_square(img)
-
-    
-
-
-
 
 def crop_image_square(img):
-    # print("Current Image Size [","Height:",img.shape[0]," Width:", img.shape[1]," ]")
 
     resized_image = 0
 
     if img.shape[0] < img.shape[1]:
         resized_image = img[:int(img.shape[0]),int((img.shape[1]/2)-(img.shape[0]/2)):int((img.shape[1]/2)+(img.shape[0]/2))]
-
-    # print("Cropped Image Size[","Height:",resized_image.shape[0]," Width:", resized_image.shape[1]," ]")
 
     return resized_image
 
