0a7e4d7cd6197fae830ba7b65e92408806c962c5
Helmika Priyanto <helmika.zero@gmail.com>
2022-07-05 17:37:11 +0700

    Update proghedec_nocrop.py

--- proghedec_nocrop.py
+++ proghedec_nocrop.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from logging import warning
 import cv2
 import torch
@@ -88,9 +89,9 @@ def main():
     conf_thres = input('Confidence Threshold =')
     iou_thres = input('IOU for NMS threshold =')
 
-    if conf_thres == '':
+    if conf_thres == NULL:
         conf_thres = 0.25
-    if iou_thres == '':
+    if iou_thres == NULL:
         iou_thres = 0.45
     
     conf_thres = float(conf_thres)
